c17f08481fd9d1dcb5b51b5bae2b869ff6c24d20
Jayden Rasband <jr2swim@gmail.com>
2022-02-11 23:53:48 -0700

    Updated package files
    got rid of class to simplify imports. added details to __init__.py. Version Updates.

--- pandas_geojson/__init__.py
+++ pandas_geojson/__init__.py
@@ -1,1 +1,18 @@
-__version__ = "1.0.0"
+__version__ = "1.1.0"
+
+from pandas_geojson import (
+    read_geojson,
+    read_geojson_url,
+    to_geojson,
+    write_geojson,
+    filter_geojson,
+)
+
+
+__all__ = [
+    "read_geojson",
+    "read_geojson_url",
+    "to_geojson",
+    "write_geojson",
+    "filter_geojson",
+    ]
--- pandas_geojson/pandas_geojson.py
+++ pandas_geojson/pandas_geojson.py
@@ -1,55 +1,50 @@
 import json
 from urllib.request import urlopen
 
-class GeoJSON():
-    def to_geojson(self, df, lat, lon, properties):
+def to_geojson(df, lat, lon, properties):
 
-        geojson = {'type': 'FeatureCollection', 'features': []}
-        for _, row in df.iterrows():
-            # create a feature template to fill in
-            feature = {'type': 'Feature',
-                       'properties': {},
-                       'geometry': {'type': 'Point',
-                                    'coordinates': []}}
+    geojson = {'type': 'FeatureCollection', 'features': []}
+    for _, row in df.iterrows():
+        feature = {'type': 'Feature',
+                   'properties': {},
+                   'geometry': {'type': 'Point',
+                                'coordinates': []}}
 
-            # fill in the coordinates
-            feature['geometry']['coordinates'] = [row[lon], row[lat]]
+        feature['geometry']['coordinates'] = [row[lon], row[lat]]
 
-            # for each column, get the value and add it as a new feature property
-            for prop in properties:
-                feature['properties'][prop] = row[prop]
+        for prop in properties:
+            feature['properties'][prop] = row[prop]
 
-            # add this feature (aka, converted dataframe row) to the list of features inside our dict
-            geojson['features'].append(feature)
+        geojson['features'].append(feature)
 
-        return geojson
+    return geojson
 
-    def write_geojson(self, geo_json, filename):
-        with open(filename, 'w', encoding='utf8') as f:
-            f.write(geo_json)
-            
-            
-    def filter_geojson(self,geo_json, filter_list, property_key):
-        filtered = []
-        for ft in geo_json['features']:
-            if ft['properties'][f'{property_key}'] in filter_list:
-                filtered.append(ft)
-        # print(len(filtered))
+def write_geojson(geo_json, filename):
+    with open(filename, 'w', encoding='utf8') as f:
+        f.write(geo_json)
 
-        newgeojson = {}
-        newgeojson['type'] = 'FeatureCollection'
-        newgeojson['features'] = filtered
-        newgeojson = json.dumps(newgeojson)
 
-        return newgeojson
+def filter_geojson(geo_json, filter_list, property_key):
+    filtered = []
+    for ft in geo_json['features']:
+        if ft['properties'][f'{property_key}'] in filter_list:
+            filtered.append(ft)
+    # print(len(filtered))
 
-    def read_geojson(self,filename):
-        with open(f'{filename}') as response:
-            geo_json = json.load(response)
-        return geo_json
+    newgeojson = {}
+    newgeojson['type'] = 'FeatureCollection'
+    newgeojson['features'] = filtered
+    newgeojson = json.dumps(newgeojson)
 
-    def read_geojson_url(self, url):
-        with urlopen(f'{url}') as response:
-            geo_json = json.load(response)
+    return newgeojson
 
-        return geo_json
+def read_geojson(filename):
+    with open(f'{filename}') as response:
+        geo_json = json.load(response)
+    return geo_json
+
+def read_geojson_url(url):
+    with urlopen(f'{url}') as response:
+        geo_json = json.load(response)
+
+    return geo_json
